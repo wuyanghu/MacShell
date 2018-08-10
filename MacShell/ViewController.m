d0ba7ac7bea4daf1d47493504dcaef72b2c01e96
ruantong <823105162@qq.com>
2018-08-10 15:16:39 +0800

    4

--- MacShell/ViewController.m
+++ MacShell/ViewController.m
@@ -33,6 +33,7 @@
     //1
     //2
     //3
+    //4
     [self.chooseDirectoryButton setTitle:self.chooseFilePath?self.chooseFilePath:@"请选择目录"];
     // Do any additional setup after loading the view.
     self.chensongButtonCell.state = [self.auditPersonDictionary[kChensong] intValue];
