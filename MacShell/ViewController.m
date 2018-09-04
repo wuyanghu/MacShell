62c27cf95baea6561536bfcfde17feae7a06b784
ruantong <823105162@qq.com>
2018-09-04 18:59:30 +0800

    9

--- MacShell/ViewController.m
+++ MacShell/ViewController.m
@@ -64,7 +64,7 @@
             [self.cacheLabelDict setObject:label forKey:key1];
         }
     }];
-    //amend123456
+    //amend123456789
 }
 
 - (void)setRepresentedObject:(id)representedObject {
