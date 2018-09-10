192027a07a5254f27df9838c2ffb5985ab61c161
ruantong <823105162@qq.com>
2018-09-10 10:41:32 +0800

    1234

--- MacShell/ViewController.m
+++ MacShell/ViewController.m
@@ -149,7 +149,7 @@
     
     [self.arcCommandDirectoryButton setTitle:self.arcCommandPath?self.arcCommandPath:NSLocalizedString(@"chooseArcPath", nil)];
     [self.chooseDirectoryButton setTitle:self.chooseFilePath?self.chooseFilePath:NSLocalizedString(@"chooseProjectPath", nil)];
-    //12
+    //1234
 }
 
 - (void)setRepresentedObject:(id)representedObject {
