c518027ed9502b0123ff82483ff23243e3befc71
ruantong <823105162@qq.com>
2018-09-21 15:12:12 +0800

    Revert "del 1"
    This reverts commit 079e81e4f575a7567e85e99c4873ea7b148d2799.

--- MacShell/ViewController.m
+++ MacShell/ViewController.m
@@ -149,7 +149,7 @@
     
     [self.arcCommandDirectoryButton setTitle:self.arcCommandPath?self.arcCommandPath:NSLocalizedString(@"chooseArcPath", nil)];
     [self.chooseDirectoryButton setTitle:self.chooseFilePath?self.chooseFilePath:NSLocalizedString(@"chooseProjectPath", nil)];
-    //123456789
+    //12345678
 }
 
 - (void)setRepresentedObject:(id)representedObject {
